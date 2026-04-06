6cdcc56c47329fd195c5f729ccfa7c05d5393461
HDegroote <75906619+HDegroote@users.noreply.github.com>
2026-04-06 15:58:53 +0200

    (breaking) Support bare (#5)

--- bin.mjs
+++ bin.mjs
@@ -1,103 +1,102 @@
 #!/usr/bin/env node
 
 import path from 'path'
-import { program, createOption } from 'commander'
+import { command, flag } from 'paparam'
 import id from 'hypercore-id-encoding'
 import DHT from 'hyperdht'
 import Corestore from 'corestore'
 import { Server as RelayServer } from 'blind-relay'
-import replSwarm from 'repl-swarm'
 import Instrumentation from 'hyper-instrument'
-import pino from 'pino'
 import goodbye from 'graceful-goodbye'
 import packageInfo from './package.json' with { type: 'json' }
 
 const SERVICE_NAME = 'blind-relay'
+const DEFAULT_STORAGE = './corestore'
+const DEFAULT_PORT = 49737
 
-program
-  .addOption(createOption('-s, --storage <path>').default('./corestore'))
-  .addOption(createOption('-p, --port <num>').default(49737).argParser(Number))
-  .addOption(createOption('-r, --repl').default(false))
-  .addOption(createOption('--scraper-public-key <str>').default(null))
-  .addOption(createOption('--scraper-secret <str>').default(null))
-  .addOption(createOption('--scraper-alias <str>').default(null))
-  .action(action)
-  .parseAsync()
-  .catch((err) => {
-    console.error(`error: ${err.message}`)
-    process.exitCode = 1
-  })
+const cmd = command(
+  'blind-relay',
+  flag('--storage|-s [path]'),
+  flag('--port|-p [int]'),
+  flag(
+    '--scraper-public-key [scraper-public-key]',
+    'Public key of a dht-prometheus scraper.  Can be hex or z32.'
+  ),
+  flag(
+    '--scraper-secret [scraper-secret]',
+    'Secret of the dht-prometheus scraper.  Can be hex or z32.'
+  ),
+  flag('--scraper-alias [scraper-alias]', '(optional) Alias with which to register to the scraper'),
+  async function ({ flags }) {
+    const logger = console // TODO: move back to pino
+    const storage = flags.storage || DEFAULT_STORAGE
+    const port = flags.port ? parseInt(flags.port) : DEFAULT_PORT
 
-async function action(opts) {
-  const logger = pino({ name: SERVICE_NAME })
+    const { scraperPublicKey, scraperSecret, scraperAlias } = flags
 
-  const corestoreLoc = path.resolve(opts.storage)
-  logger.info(`Using corestore storage at ${corestoreLoc}`)
-  const store = new Corestore(corestoreLoc)
+    const corestoreLoc = path.resolve(storage)
+    logger.info(`Using corestore storage at ${corestoreLoc}`)
+    const store = new Corestore(corestoreLoc)
 
-  const dht = new DHT({ port: opts.port })
+    const dht = new DHT({ port })
 
-  const relay = new RelayServer({
-    createStream(opts) {
-      return dht.createRawStream({ ...opts, framed: true })
-    }
-  })
-
-  const server = dht.createServer((socket) => {
-    socket.setKeepAlive(5000)
-
-    socket.on('error', noop)
-
-    const session = relay.accept(socket, { id: socket.remotePublicKey })
-    session.on('error', noop)
-  })
-
-  let instrumentation = null
-  goodbye(async () => {
-    if (instrumentation) {
-      logger.info('Closing instrumentation')
-      await instrumentation.close()
-    }
-    logger.info('Shutting down blind relay')
-    await dht.destroy()
-    await store.close()
-    logger.info('Shut down blind relay')
-  })
-
-  await server.listen(await store.createKeyPair('blind-relay'))
-
-  if (opts.repl) {
-    replSwarm({ dht, relay, server })
-  }
-
-  if (opts.scraperPublicKey) {
-    logger.info('Setting up instrumentation')
-
-    const scraperPublicKey = id.decode(opts.scraperPublicKey)
-    const scraperSecret = id.decode(opts.scraperSecret)
-
-    let prometheusAlias = opts.scraperAlias
-    if (prometheusAlias && prometheusAlias.length > 99) {
-      throw new Error('The Prometheus alias must have length less than 100')
-    }
-    if (!prometheusAlias) {
-      prometheusAlias = `${SERVICE_NAME}-${id.normalize(server.publicKey)}`.slice(0, 99)
-    }
-
-    instrumentation = new Instrumentation({
-      dht,
-      scraperPublicKey,
-      prometheusAlias,
-      scraperSecret,
-      prometheusServiceName: SERVICE_NAME,
-      version: packageInfo.version
+    const relay = new RelayServer({
+      createStream(opts) {
+        return dht.createRawStream({ ...opts, framed: true })
+      }
     })
 
-    instrumentation.registerLogger(logger)
-    await instrumentation.ready()
-  }
+    const server = dht.createServer((socket) => {
+      socket.setKeepAlive(5000)
 
-  logger.info(`Server listening on ${id.encode(server.publicKey)}`)
-}
+      socket.on('error', noop)
+
+      const session = relay.accept(socket, { id: socket.remotePublicKey })
+      session.on('error', noop)
+    })
+
+    let instrumentation = null
+    goodbye(async () => {
+      if (instrumentation) {
+        logger.info('Closing instrumentation')
+        await instrumentation.close()
+      }
+      logger.info('Shutting down blind relay')
+      await dht.destroy()
+      await store.close()
+      logger.info('Shut down blind relay')
+    })
+
+    await server.listen(await store.createKeyPair('blind-relay'))
+
+    if (scraperPublicKey) {
+      logger.info('Setting up instrumentation')
+
+      let prometheusAlias = scraperAlias
+      if (prometheusAlias && prometheusAlias.length > 99) {
+        throw new Error('The Prometheus alias must have length less than 100')
+      }
+      if (!prometheusAlias) {
+        prometheusAlias = `${SERVICE_NAME}-${id.normalize(server.publicKey)}`.slice(0, 99)
+      }
+
+      instrumentation = new Instrumentation({
+        dht,
+        scraperPublicKey,
+        prometheusAlias,
+        scraperSecret,
+        prometheusServiceName: SERVICE_NAME,
+        version: packageInfo.version
+      })
+
+      instrumentation.registerLogger(logger)
+      await instrumentation.ready()
+    }
+
+    logger.info(`Server listening on ${id.encode(server.publicKey)}`)
+  }
+)
 
 function noop() {}
+
+cmd.parse()
